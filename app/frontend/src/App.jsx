7fdbebd1dad8488c539a6c5326dd41d2990a2ed6
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-01-02 00:42:46 +0530

    feat : our Team page

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HomePage from "./pages/home";
 import Price from "./pages/price";
+import OurTeam from "./pages/ourTeam";
 
 function App() {
   return (
     <>
       {/* <HomePage /> */}
-      <Price />
+      <OurTeam />
     </>
   );
 }
