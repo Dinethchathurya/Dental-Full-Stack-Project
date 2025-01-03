2ed8a246cca8fc085f25686d57e17f61eac9cf9c
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-01-04 01:48:44 +0530

    feat : add mounts to compose file using volumes

--- app/frontend/src/pages/AdminDashboard.jsx
+++ app/frontend/src/pages/AdminDashboard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Sidebar from "../components/admin/Sidebar"
-import Navbar from "../components/navbar";
-import Appointments from "../components/admin/Appointments";
-import AddDoctorModal from "../components/admin/AddDoctorModal";
-import PriceList from "../components/admin/PriceList";
+import Sidebar from "../components/admin/sidebar.jsx"
+import Navbar from "../components/navbar.jsx";
+import Appointments from "../components/admin/Appointments.jsx";
+import AddDoctorModal from "../components/admin/AddDoctorModal.jsx";
+import PriceList from "../components/admin/PriceList.jsx";
 import { Helmet } from 'react-helmet-async';
 
 
